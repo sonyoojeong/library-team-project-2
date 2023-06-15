046d88eb346e0264ef46847ecba9697d0ba1b4a9
Woongcor <117280276+Woongcor@users.noreply.github.com>
2023-06-15 15:34:21 +0900

    커밋 취소
    게시판 컨트롤러 메서드 매핑명 중복으로 수정

--- src/main/java/com/library/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-
-import com.library.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File,Long> {}
